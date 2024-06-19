428254424a52ba31b4265305448790ae5dc8a05e
TechBuilderTeam <saashid18@gmail.com>
2024-06-19 23:55:21 +0600

    02_Digital-clock

---
+++ digital_clock/Digital_clock.js
@@ -0,0 +1,26 @@
+const time = document.getElementById('time');
+const timeFormat = document.getElementById('timeformat');
+
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(showTime, 1000);
+});
+console.log(time);
+const showTime = () => {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    h = ((h < 10) ? `0${h}` : h);
+    m = (m < 10) ? `0${m}` : m;
+    s = (s < 10) ? `0${s}` : s;
+
+
+    timeFormat.innerHTML = `${h > 12 ? 'PM' : 'AM'}`;
+    if (h > 12 && h < 24) {
+        h = h - 12;
+    }
+    time.innerHTML = `${h} : ${m} : ${s}`;
+
+
+};
