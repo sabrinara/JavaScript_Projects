cead560016a5b5442992fa92197d17b84eaa7adb
TechBuilderTeam <saashid18@gmail.com>
2024-06-21 01:59:57 +0600

    02_digital_clock

--- digital_clock/Digital_clock.js
+++ digital_clock/Digital_clock.js
@@ -11,15 +11,18 @@ const showTime = () => {
     let m = date.getMinutes();
     let s = date.getSeconds();
 
+    timeFormat.innerHTML = `${h > 12 ? 'PM' : 'AM'}`;
+    
+    if (h > 12 && h < 24) {
+        h = h - 12;
+    }
+
     h = ((h < 10) ? `0${h}` : h);
     m = (m < 10) ? `0${m}` : m;
     s = (s < 10) ? `0${s}` : s;
 
 
-    timeFormat.innerHTML = `${h > 12 ? 'PM' : 'AM'}`;
-    if (h > 12 && h < 24) {
-        h = h - 12;
-    }
+
     time.innerHTML = `${h} : ${m} : ${s}`;
 
 
